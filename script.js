f76fa006cfd7da39e9570abe79742721427f0042
Byte Sized Joker <rugveddinesh97@gmail.com>
2025-08-24 01:00:51 +0530

    deleted:    images/icons/account.png 	renamed:    images/icons/Buy-us-a-kofi.png -> images/icons/dark/Buy-us-a-kofi.png 	new file:   images/icons/dark/account.png 	renamed:    images/icons/cart.png -> images/icons/dark/cart.png 	renamed:    images/icons/home.png -> images/icons/dark/home.png 	renamed:    images/icons/logo.png -> images/icons/dark/logo.png 	new file:   images/icons/dark/logo1.png 	renamed:    images/icons/support.png -> images/icons/dark/support.png 	new file:   images/icons/dark/theme-mode.png 	new file:   images/icons/light/account.png 	new file:   images/icons/light/buy-us-a-kofi.png 	new file:   images/icons/light/cart.png 	new file:   images/icons/light/home.png 	new file:   images/icons/light/logo.png 	new file:   images/icons/light/logo1.png 	new file:   images/icons/light/support.png 	new file:   images/icons/light/theme-mode.png 	modified:   main.html 	modified:   pages/Products/accessories.html 	modified:   pages/Products/console-section.html 	modified:   pages/Products/laptop-section.html 	modified:   pages/Products/pc-section.html 	modified:   pages/Products/phone-table-section.html 	modified:   pages/about.html 	modified:   pages/account.html 	modified:   pages/buying-guide.html 	modified:   pages/cart.html 	modified:   pages/help.html 	modified:   pages/privacy.html 	modified:   pages/tnc.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
-//*     Main JavaScript
+//* ------------------- Main JavaScript -------------------
 
-// Call convertTo169() on page load
-window.onload = convertTo169();
-
-// Get screen width and height
+// Get browser screen width and height
 const defaultWidth = innerWidth - 8;
 const defaultHeight = innerHeight - 8;
 
-// Check if convertTo169() was called or width and height were stored previously
+// If screen dimensions not are saved in localStorage then calculate 16:9 ratio on load
 if (
     !localStorage.getItem("screenWidth") ||
     !localStorage.getItem("screenHeight")
 ) {
-    localStorage.setItem("screenWidth", defaultWidth + "px");
-    localStorage.setItem("screenHeight", defaultHeight + "px");
+    window.onload = convertTo169();
 }
 
-// Apply stored screen width as a CSS variable
+// Load previously saved theme, or set theme to dark mode if not perviously saved
+if (!localStorage.getItem("theme")) {
+    setTheme("dark");
+    localStorage.setItem("theme", "dark");
+} else if (localStorage.getItem("theme") == "light") {
+    setTheme("light");
+    localStorage.setItem("theme", "light");
+} else if (localStorage.getItem("theme") == "dark") {
+    setTheme("dark");
+    localStorage.setItem("theme", "dark");
+}
+
+// Wait for Document Object Model (DOM) to load and then update all icons
+document.addEventListener("DOMContentLoaded", updateAllIcons);
+
+//! ------------------- Danger Zone -------------------
+// Apply stored width/height from localStorage as CSS variables
+// Ensures layout remains constant across reloads
 const screenWidth = localStorage.getItem("screenWidth");
 const screenHeight = localStorage.getItem("screenHeight");
 document.documentElement.style.setProperty("--screen-width", screenWidth);
 document.documentElement.style.setProperty("--screen-height", screenHeight);
 
-//# functions
-//* Convert to 16:9 ratio
+//# ------------------- Functions -------------------
+
+//* Convert to ~16:9 aspect ratio (with tolerance)
+// Stores new dimensions in localStorage and updates CSS variables
 function convertTo169() {
     // Get screen size
     let scrWidth = window.innerWidth;
     let scrHeight = window.innerHeight;
 
     // Define required ratio
-    let requiredRatio = (16 / 9) * 1.105; // 110.5% of 16:9 ratio
+    let requiredRatio = (16 / 9) * 1.225; // 122.5% of 16:9 ratio
 
     // Current ratio
     let currentRatio = scrWidth / scrHeight;
@@ -41,7 +56,7 @@ function convertTo169() {
     // Check if screen is wider(if) or taller(else)
     if (currentRatio > requiredRatio) {
         newHeight = scrHeight;
-        newWidth = newHeight * requiredRatio;
+        newWidth = Math.round(newHeight * requiredRatio);
     } else {
         newWidth = scrWidth;
         newHeight = newWidth / requiredRatio;
@@ -53,7 +68,7 @@ function convertTo169() {
         newHeight = newWidth / requiredRatio;
     } else if (newWidth < 900) {
         newWidth = 900;
-        newHeight = newWidth * requiredRatio;
+        newHeight = Math.round(newWidth * requiredRatio);
     }
 
     //? Debug: Print new width and height
@@ -75,25 +90,199 @@ function convertTo169() {
     );
 }
 
-//# Debug functions
-//? Debug: Override screen width (Console Command)
-function overrideWidth(newWidth) {
-    if (newWidth == null) {
-        // Check if input is empty
-        console.log("Error empty width input");
-        return;
-    } else if (newWidth == "Reset") {
-        // Check if user wants to reset width
-        newWidth = defaultWidth;
-    }
-    // Set new width
-    const overriddenWidth = newWidth + "px";
-    localStorage.setItem("screenWidth", overriddenWidth);
-    document.documentElement.style.setProperty(
-        "--screen-width",
-        overriddenWidth
-    );
+//* Get current theme from localStorage
+function getTheme() {
+    return localStorage.getItem("theme");
+}
 
-    console.log("Screen width overridden to: " + overriddenWidth);
-    console.log("Default screen width is: " + screenWidth);
+//* Set theme colors (light/dark) by updating CSS variables
+// Also call icon update to apply themed icons
+function setTheme(theme) {
+    if (theme == "light") {
+        localStorage.setItem("theme", "light");
+
+        document.documentElement.style.setProperty("--bg-color", "#e0e0e0ff");
+        document.documentElement.style.setProperty("--text-color", "#2e2e2e");
+        document.documentElement.style.setProperty("--hr-color", "#bdbdbd");
+        document.documentElement.style.setProperty(
+            "--table-bg-color",
+            "#ffffff"
+        );
+        document.documentElement.style.setProperty(
+            "--info-bg-color",
+            "#fff8d6"
+        );
+        document.documentElement.style.setProperty(
+            "--info-text-color",
+            "#2e2e2e"
+        );
+        document.documentElement.style.setProperty(
+            "--input-caption-color",
+            "rgba(37, 37, 37, 0.7)"
+        );
+    } else if (theme == "dark") {
+        localStorage.setItem("theme", "dark");
+
+        document.documentElement.style.setProperty("--bg-color", "#181a1b");
+        document.documentElement.style.setProperty("--text-color", "#d5d3cf");
+        document.documentElement.style.setProperty("--hr-color", "#e9e6f0");
+        document.documentElement.style.setProperty(
+            "--table-bg-color",
+            "#252828"
+        );
+        document.documentElement.style.setProperty(
+            "--info-bg-color",
+            "#d5d3cf"
+        );
+        document.documentElement.style.setProperty(
+            "--info-text-color",
+            "#252828"
+        );
+        document.documentElement.style.setProperty(
+            "--input-caption-color",
+            "rgba(192, 192, 192, 0.7)"
+        );
+    } else {
+        console.log("Invalid theme");
+        console.log("Available themes: light, dark");
+    }
+
+    updateAllIcons();
+}
+
+//* Toggle between dark and light theme
+// Called by theme button in the document
+function toggleTheme() {
+    console.log("Toggling theme");
+    let theme = getTheme();
+    if (theme == "light") {
+        setTheme("dark");
+    } else if (theme == "dark") {
+        setTheme("light");
+    }
+
+    const themeButton = document.getElementById("mode");
+    themeButton.src = setIconPath("theme-mode.png");
+}
+
+//* Update all icons on the page based on theme
+// Loops through predefined IDs and replaces their src paths
+function updateAllIcons() {
+    const iconIDs = [
+        "account",
+        "buy-us-a-kofi",
+        "cart",
+        "home",
+        "logo",
+        "logo1",
+        "support",
+        "theme-mode",
+    ];
+
+    for (let i = 0; i < iconIDs.length; i++) {
+        updateIcon(iconIDs[i], iconIDs[i] + ".png");
+        console.log(iconIDs[i] + ", " + iconIDs[i] + ".png");
+    }
+}
+
+//* Update a single icon element by ID
+// Checks if element is null or not before applying new src path
+function updateIcon(elementID, iconName) {
+    const element = document.getElementById(elementID);
+    if (!element) {
+        console.warn(`No element with id="${elementID}" found`);
+        return;
+    }
+    element.src = setIconPath(iconName);
+    console.log(`Icon updated for #${elementID} → ${element.src}`);
+}
+
+//* Build the correct file path for an icon based on theme
+// Returns full path in string
+function setIconPath(iconName) {
+    const theme = getTheme();
+
+    let folderPath = "/images/icons/";
+
+    if (theme == "light") {
+        folderPath = folderPath + "light/";
+        console.log("Light folder Path: " + folderPath);
+    } else if (theme == "dark") {
+        folderPath = folderPath + "dark/";
+        console.log("Dark folder Path: " + folderPath);
+    } else {
+        console.log("Invalid theme");
+        console.log("Available themes: light, dark");
+    }
+
+    console.log("Icon Path: " + folderPath);
+    return folderPath + iconName;
+}
+
+//# ------------------- Debugging Functions -------------------
+
+//? Debug: Directly set screen dimensions
+function setScreenDimensions(width, height) {
+    document.documentElement.style.setProperty("--screen-width", width + "px");
+    document.documentElement.style.setProperty(
+        "--screen-height",
+        height + "px"
+    );
+}
+
+//? Debug: Print current screen dimensions to console (from localStorage)
+function getScreenDimensions() {
+    console.log("Screen Dimensions are: " + screenWidth + " x " + screenHeight);
+}
+
+//? Debug: Override screen dimensions and save them in localStorage
+function overrideScreenDimensions(newWidth, newHeight) {
+    if (newWidth == null || newHeight == null) {
+        // Check if input is empty
+        console.log("Error empty input");
+        return;
+    } else if (newWidth == 0 || newHeight == 0) {
+        resetScreenDimensions();
+        return;
+    }
+
+    const overriddenWidth = newWidth + "px";
+    const overriddenHeight = newHeight + "px";
+    localStorage.setItem("screenWidth", overriddenWidth);
+    localStorage.setItem("screenHeight", overriddenHeight);
+    setScreenDimensions(overriddenWidth, overriddenHeight);
+
+    console.log(
+        "Screen Dimensions overridden to: " +
+            overriddenWidth +
+            " x " +
+            overriddenHeight
+    );
+    console.log(
+        "Default screen Dimensions are: " + screenWidth + " x " + screenHeight
+    );
+}
+
+//? Debug: Reset stored dimensions back to default
+function resetScreenDimensions() {
+    localStorage.removeItem("screenWidth");
+    localStorage.removeItem("screenHeight");
+    setScreenDimensions(defaultWidth, defaultHeight);
+}
+
+//? Debug: Print all available commands
+function help() {
+    console.log("Available Commands:");
+    console.log("convertTo169()");
+    console.log("getTheme()");
+    console.log("setTheme(theme) - theme: light, dark");
+    console.log("toggleTheme()");
+    console.log("updateAllIcons()");
+    console.log(
+        "updateIcon(elementID, iconName) - elementID: string (without #), iconName: string (without extension)"
+    );
+    console.log("setScreenDimensions(width, height)");
+    console.log("getScreenDimensions()");
+    console.log("overrideScreenDimensions(newWidth, newHeight)");
+    console.log("resetScreenDimensions()");
 }
